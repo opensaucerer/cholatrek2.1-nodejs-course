e84022da5393d5e2753a94d4780c2f7ef6bcb0b2
Perfection Loveday <lovedayperfection1@gmail.com>
2021-03-10 20:33:34 +0100

    finished work on module18

--- project/wordy/app.js
+++ project/wordy/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const { mainRouter } = require('./routes/mainRoutes');
+const { postRouter } = require('./routes/postRoutes');
 const { Post } = require('./models/post');
 const bodyParser = require('body-parser');
 
@@ -26,98 +28,8 @@ mongoose
   })
   .catch((error) => console.log(error));
 
-//   homepage
-app.get('/', (req, res) => {
-  Post.find()
-    .sort({ createdAt: -1 })
-    .then((result) => {
-      //   console.log(result);
-      res.render('home', { posts: result });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
+// Everything related to created, reading, updating, and deleting, posts
+app.use('/posts', postRouter);
 
-// new post
-app.get('/new_post', (req, res) => {
-  res.render('new_post');
-});
-
-// publish new post
-app.post('/new_post', async (req, res) => {
-  // save data to the database
-  try {
-    const post = new Post(req.body);
-    result = await post.save();
-    res.redirect('/');
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// retrieve single post
-app.get('/posts/read/:id', (req, res) => {
-  const id = req.params.id;
-  //   console.log(id);
-  Post.findById(id)
-    .then((result) => {
-      res.render('post', { post: result });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-  //   res.render('new_post');
-});
-
-// updating a post
-app.get('/update_post/:id', (req, res) => {
-  const id = req.params.id;
-  Post.findById(id)
-    .then((result) => {
-      res.render('update_post', { post: result });
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-});
-
-// API for updating our post
-app.put('/update_post/:id', (req, res) => {
-  const id = req.params.id;
-  // console.log(req.body);
-  Post.findByIdAndUpdate(id, req.body)
-    .then((result) => {
-      res.json({
-        status: true,
-        message: 'Post updated successfully',
-        redirect: '/',
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        status: false,
-        error: 'Post update failed',
-      });
-    });
-});
-
-// API for deleting post
-app.delete('/delete_post/:id', (req, res) => {
-  const id = req.params.id;
-
-  Post.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({
-        status: true,
-        message: 'Post deleted successfully',
-        redirect: '/',
-      });
-    })
-    .catch((error) => {
-      res.status(400).json({
-        status: false,
-        error: 'Post delete failed',
-      });
-    });
-});
+// everything relating to home, about, and 404
+app.use(mainRouter);
